4dffb6b960f0fd191d0d04f021a99d18b0ee66b2
Maatouf Saad <saadmaatouf4@gmail.com>
2023-08-28 17:35:41 +0100

    Home Screen

--- app/index.js
+++ app/index.js
@@ -1,11 +1,42 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES} from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+    
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightwhite }} >
+            <Stack.Screen 
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightwhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" onPress={() => {} } />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" onPress={() => {} } />
+                    ),
+                    headerTitle: ""
+                }}/>
+            <ScrollView showsVerticalScrollIndicator={false} >
+                <View 
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium,
+                    }} >
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+                
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default Home;
